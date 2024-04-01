91c97639ffc3d183d25d3894ff372ec24ac70d8f
Daniel Teles Brito <dtdanielteles@gmail.com>
2024-04-01 11:06:42 -0300

    Olá, munfo

--- src/aula01/HelloWorld.java
+++ src/aula01/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
     public static void main(String [] args){
         // Declaração de variáveis
         String nome;
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
 
         /*
         Saída em texto
