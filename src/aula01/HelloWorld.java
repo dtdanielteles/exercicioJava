ac503fb9c2d57639211cee8111f9b0248ba62f19
Daniel Teles Brito <dtdanielteles@gmail.com>
2024-04-01 10:51:58 -0300

    Olá, munfo

--- src/aula01/HelloWorld.java
+++ src/aula01/HelloWorld.java
@@ -1,7 +1,16 @@
 package aula01;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.println("Olá, Daniel!");
+        String nome;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+
+        nome = sc.next();
+
+        System.out.println("Olá, " + nome + "! Seja bem-vindo!");
     }
 }
