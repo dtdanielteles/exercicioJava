18500abdb71a1ba156c202765c47684eae42d58a
Daniel Teles Brito <dtdanielteles@gmail.com>
2024-04-01 11:06:22 -0300

    Olá, munfo

--- src/aula01/HelloWorld.java
+++ src/aula01/HelloWorld.java
@@ -1,16 +1,23 @@
 package aula01;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class HelloWorld {
     public static void main(String [] args){
+        // Declaração de variáveis
         String nome;
         Scanner sc = new Scanner(System.in);
 
+        /*
+        Saída em texto
         System.out.println("Digite seu nome: ");
-
         nome = sc.next();
-
         System.out.println("Olá, " + nome + "! Seja bem-vindo!");
+        */
+
+        // Saída gráfica
+        nome = JOptionPane.showInputDialog("Qual o seu nome? ");
+        JOptionPane.showMessageDialog(null, "Olá, " + nome + "! Seja bem-vindo!");
     }
 }
